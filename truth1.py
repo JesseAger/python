0bd83e884523adad2374a0549f7f2cd50b47343f
JesseAger <agerjesse6@gmail.com>
2023-05-24 15:54:53 +0300

    initial

--- truth1.py
+++ truth1.py
@@ -8,8 +8,23 @@ print(character.upper())
 print(pow(my_num, 3))
 print(ceil(numero))
 print(floor(numero))
-code= input("Enter the code:")
-print("The " + name + " verification code is " + code)
+OTP= input("Enter your OneTimePassword:")
+print("The " + name + " verification OTP is " + OTP)
 print("Roses are {colour}")
 print("{Plural} are blue")
 print("I love {celebrity}")
+Client = ["Jimmy", "Jayden", "Jt", "Mercy", "Frank"]
+Codes = [4004, 2909, 6998, 1536, 3302]
+
+if Client.index("Jimmy") == 0:
+    # Your code here for when the first client is "Jimmy"
+
+    print("Jimmy's code is " + Codes.index("4004"))
+else:
+    # Your code here for when the first client is not "Jimmy"
+    print("First client is not Jimmy")
+
+
+     
+
+
