0da8f158fa5973d9c71091b6df156b854c813386
JesseAger <agerjesse6@gmail.com>
2023-05-24 13:31:35 +0300

    initial

---
+++ truth1.py
@@ -0,0 +1,15 @@
+from math import*
+character = "giraffe academy"
+my_num = 4
+numero = 2.6
+print(character. replace("giraffe", "Light"))
+name = (character. replace("giraffe", "Light"))
+print(character.upper())
+print(pow(my_num, 3))
+print(ceil(numero))
+print(floor(numero))
+code= input("Enter the code:")
+print("The " + name + " verification code is " + code)
+print("Roses are {colour}")
+print("{Plural} are blue")
+print("I love {celebrity}")
